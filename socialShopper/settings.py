6106b899005acb9dc3dc2fbdf7af0947416f59cf
chinmaynaveen20 <chinmaynaveen20@gmail.com>
2022-10-09 09:52:38 -0700

    production related changes

--- socialShopper/settings.py
+++ socialShopper/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#thn3k7-gb_f=p%5x06)csrq!ir&_yz_yqexe+s5j%=7pr2*_i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
